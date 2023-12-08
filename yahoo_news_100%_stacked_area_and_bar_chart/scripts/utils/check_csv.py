518e34fda1d81da062b991f839ba0bf90e371a31
yuu <crosscore.g@gmail.com>
2023-12-08 17:57:49 +0900

    add json analysis

--- yahoo_news_100%_stacked_area_and_bar_chart/scripts/utils/check_csv.py
+++ yahoo_news_100%_stacked_area_and_bar_chart/scripts/utils/check_csv.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv('../../data/csv/add_viewing_time/device_add_days_viewing_time.csv', dtype={'user': str})
+#df = pd.read_csv('../../data/csv/add_viewing_time/device_add_days_viewing_time.csv', dtype={'user': str})
+df = pd.read_csv('../../data/csv/original/device_original_with_category.csv', dtype={'user': str})
 
-print(f"df['days'].min():{df['days'].min()}")
-print(f"df['days'].max():{df['days'].max()}")
+# print(f"df['days'].min():{df['days'].min()}")
+# print(f"df['days'].max():{df['days'].max()}")
 
-#dfの'user'毎のdaysの最大値を表示
-print(df.groupby('user')['days'].max())
+##dfの'user'毎のdaysの最大値を表示
+#print(df.groupby('user')['days'].max())
 
 #dfのnanの列を表示する
-print(df.isnull().any())
 print(df.isnull().sum())
 print(df)
